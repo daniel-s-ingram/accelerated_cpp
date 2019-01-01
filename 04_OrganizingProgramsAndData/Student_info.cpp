bced298a1c5616611b162274c76671f969f6222e
Daniel Ingram <ingramds@appstate.edu>
2019-01-01 14:20:30 -0500

    Chapter 4

---
+++ 04_OrganizingProgramsAndData/Student_info.cpp
@@ -0,0 +1,34 @@
+#include "Student_info.h"
+
+using std::istream;
+using std::vector;
+
+istream& read_hw(istream& in, vector<double>& hw)
+{
+    if (in)
+    {
+        hw.clear();
+
+        double x;
+        while (in >> x)
+        {
+            hw.push_back(x);
+        }
+
+        in.clear();
+    }
+    return in;
+}
+
+
+istream& read(istream& is, Student_info& s)
+{
+    is >> s.name >> s.midterm >> s.final;
+    read_hw(is, s.homework);
+    return is;
+}
+
+bool compare(const Student_info& x, const Student_info& y)
+{
+    return x.name < y.name;
+}
